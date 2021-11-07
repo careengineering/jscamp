916f45ab2649679a9670c672563851d7891db59b
cesurkedi <cesurkedixx@protonmail.com>
2021-11-07 19:57:06 +0300

    2021-11-07

--- src/app.js
+++ src/app.js
@@ -1,28 +1,36 @@
 console.log("Merhaba Kodlama.io")
-//JS type safe değildir.
-let dolarDun = 9.20
+
+//JS type safe değildir
 let dolarBugun = 9.30
 
-{let dolarDun=9.10}
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+
 console.log(dolarDun)
 
-//sonucu 9.30 verir. var kullanırsak bloktakini kabul ederdi.
-
-
 const euroDun = 11.2
-//euroDun=11 bu satırı yazarsak hata verir
+//euroDun = 11
+
 console.log(euroDun)
 
-//array:dizi
-//camelCasing olarak tanımlama (PascalCasing de var)
-let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"] //dizi
-let konutKredileri2=[12,"Emlak Konut Kredisi",["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]] //dizi
-
-console.log(konutKredileri)
-console.log(konutKredileri2)
-
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i=0;i<konutKredileri.length;i++)
-{console.log("<li>"+konutKredileri[i]+"</li>")}
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
 console.log("</ul>")
+
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
